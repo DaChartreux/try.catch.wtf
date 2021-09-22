cdca219a5f754a00497c55f5af87e5280d1b197c
Amit Parameshwar <the.amit.parameshwar@gmail.com>
2021-09-23 03:02:53 +0530

    Chore: Add styled-components

--- components/ui/AppThemeProvider.tsx
+++ components/ui/AppThemeProvider.tsx
@@ -1,18 +1,28 @@
-import React from "react";
+import React, { useContext } from "react";
 
 import { useAppThemeProvider } from "@hooks/useAppThemeProvider";
-import AppThemeContext from "@components/ui/AppThemeContext";
 
 type AppThemeProviderProps = {
   children: React.ReactNode;
 };
 
+const AppThemeContext = React.createContext<
+  ReturnType<typeof useAppThemeProvider>
+>({
+  appTheme: "light",
+  changeTheme: Function,
+});
+
 const AppThemeProvider = ({ children }: AppThemeProviderProps) => {
   const theme = useAppThemeProvider();
 
   return (
-    <AppThemeContext.Provider value={theme}>{children}</AppThemeContext.Provider>
+    <AppThemeContext.Provider value={theme}>
+      {children}
+    </AppThemeContext.Provider>
   );
 };
+
+export const useAppTheme = () => useContext(AppThemeContext);
 
 export default AppThemeProvider;
---
+++ styles/theme.ts
@@ -0,0 +1,10 @@
+import { DefaultTheme } from "styled-components";
+
+export const theme: DefaultTheme = {
+  borderRadius: "0.375rem",
+  fonts: {
+    fontFamily: "Roboto Mono",
+    normalWeight: 400,
+    boldWeight: 700,
+  },
+};
