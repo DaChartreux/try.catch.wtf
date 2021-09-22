06419cfa7a105dd1eade9a0fe215bf6553ab40b1
Potato Chip <thepotatoxchip@gmail.com>
2021-09-23 03:02:53 +0530

    Chore: Add styled-components

--- hooks/useAppTheme.ts
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-import AppThemeContext from "@components/ui/AppThemeContext";
-
-export const useAppTheme = <T>(light: T, dark: T) => {
-  const { appTheme } = React.useContext(AppThemeContext);
-
-  return appTheme === "light" ? light : dark;
-};
