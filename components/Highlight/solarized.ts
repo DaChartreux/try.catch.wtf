cb56a781255c272bffffafd983f7089e2550250f
Amit Parameshwar <cheemzkun@gmail.com>
2021-10-15 16:14:09 +0530

    :lipstick: chore: add custom colors for code highlight

--- components/Highlight/solarized.ts
+++ components/Highlight/solarized.ts
@@ -1,43 +1,47 @@
+import THEME from "@styles/theme";
+
 const solarized = {
   'code[class*="language-"]': {
-    color: "#f8f8f2",
+    color: `hsla(${THEME.colors["fg-100"]}, 0.65)`,
     background: "none",
+    fontWeight: "500",
     fontFamily:
-      "\"Fira Code\", Consolas, Monaco, 'Andale Mono', 'Ubuntu Mono', monospace",
+      "\"Iosevka\", Consolas, Monaco, 'Andale Mono', 'Ubuntu Mono', monospace",
     textAlign: "left",
-    whiteSpace: "pre",
+    whiteSpace: "pre-wrap",
     wordSpacing: "normal",
     wordBreak: "normal",
     wordWrap: "normal",
     lineHeight: "1.5",
-    MozTabSize: "4",
-    OTabSize: "4",
-    tabSize: "4",
+    MozTabSize: "2",
+    OTabSize: "2",
+    tabSize: "2",
     WebkitHyphens: "none",
     MozHyphens: "none",
     msHyphens: "none",
     hyphens: "none",
   },
   'pre[class*="language-"]': {
-    color: "#f8f8f2",
+    color: `hsla(${THEME.colors["fg-100"]}, 0.65)`,
+    background: "none",
+    fontWeight: "500",
     fontFamily:
-      "\"Fira Code\", Consolas, Monaco, 'Andale Mono', 'Ubuntu Mono', monospace",
+      "\"Iosevka\", Consolas, Monaco, 'Andale Mono', 'Ubuntu Mono', monospace",
     textAlign: "left",
     whiteSpace: "pre",
-    wordSpacing: "normal",
+    wordSpacing: "pre-wrap",
     wordBreak: "normal",
     wordWrap: "normal",
     lineHeight: "1.5",
-    MozTabSize: "4",
-    OTabSize: "4",
-    tabSize: "4",
+    MozTabSize: "2",
+    OTabSize: "2",
+    tabSize: "2",
     WebkitHyphens: "none",
     MozHyphens: "none",
     msHyphens: "none",
     hyphens: "none",
-    padding: "0",
+    padding: "1rem 0",
     margin: "0",
-    overflow: "auto",
   },
   ':not(pre) > code[class*="language-"]': {
     background: "#2E3440",
@@ -46,95 +50,72 @@ const solarized = {
     whiteSpace: "normal",
   },
   comment: {
-    color: "#636f88",
+    color: `hsla(${THEME.colors["fg-100"]}, 0.2)`,
   },
   prolog: {
-    color: "#636f88",
+    color: `hsla(${THEME.colors["fg-100"]}, 0.3)`,
   },
   doctype: {
-    color: "#636f88",
+    color: `hsla(${THEME.colors["fg-100"]}, 0.3)`,
   },
   cdata: {
-    color: "#636f88",
+    color: `hsla(${THEME.colors["fg-100"]}, 0.3)`,
   },
   punctuation: {
-    color: "#81A1C1",
+    color: `hsla(${THEME.colors["violet-300"]}, 1)`,
   },
   ".namespace": {
     Opacity: ".7",
   },
   property: {
-    color: "#81A1C1",
+    color: `hsla(${THEME.colors["violet-300"]}, 1)`,
   },
   tag: {
-    color: "#81A1C1",
-  },
-  constant: {
-    color: "#81A1C1",
-  },
-  symbol: {
-    color: "#81A1C1",
-  },
-  deleted: {
-    color: "#81A1C1",
+    color: `hsla(${THEME.colors["violet-300"]}, 1)`,
   },
   number: {
-    color: "#B48EAD",
+    color: `hsla(${THEME.colors["emerald-300"]}, 1)`,
   },
   boolean: {
-    color: "#81A1C1",
+    color: `hsla(${THEME.colors["emerald-300"]}, 1)`,
   },
   selector: {
     color: "#A3BE8C",
   },
   "attr-name": {
-    color: "#A3BE8C",
+    color: `hsla(${THEME.colors["emerald-300"]}, 1)`,
   },
   string: {
-    color: "#A3BE8C",
+    color: `hsla(${THEME.colors["teal-300"]}, 1)`,
   },
   char: {
-    color: "#A3BE8C",
-  },
-  builtin: {
-    color: "#A3BE8C",
-  },
-  inserted: {
-    color: "#A3BE8C",
+    color: `hsla(${THEME.colors["green-300"]}, 1)`,
   },
   operator: {
-    color: "#81A1C1",
-  },
-  entity: {
-    color: "#81A1C1",
-    cursor: "help",
-  },
-  url: {
-    color: "#81A1C1",
+    color: `hsla(${THEME.colors["fg-100"]}, 0.7)`,
   },
   ".language-css .token.string": {
-    color: "#81A1C1",
+    color: `hsla(${THEME.colors["pink-300"]}, 0.7)`,
   },
   ".style .token.string": {
-    color: "#81A1C1",
-  },
-  variable: {
-    color: "#81A1C1",
-  },
-  atrule: {
-    color: "#88C0D0",
+    color: `hsla(${THEME.colors["pink-300"]}, 0.7)`,
   },
   "attr-value": {
     color: "#88C0D0",
   },
   function: {
-    color: "#88C0D0",
+    color: `hsla(${THEME.colors["cyan-300"]}, 1)`,
+    fontStyle: "italic",
   },
   "class-name": {
-    color: "#88C0D0",
+    color: `hsla(${THEME.colors["orange-300"]}, 1)`,
   },
   keyword: {
-    color: "#81A1C1",
+    color: `hsla(${THEME.colors["pink-300"]}, 1)`,
+    fontStyle: "italic",
+  },
+  "plain-text": {
+    color: `hsla(${THEME.colors["fg-100"]}, 0.7)`,
   },
   regex: {
     color: "#EBCB8B",
