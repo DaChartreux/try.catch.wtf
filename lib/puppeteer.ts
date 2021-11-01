ab27a33dcc5da11bf8a0039da7d273ea079619a0
Amit Parameshwar <cheemzkun@gmail.com>
2021-11-01 13:30:44 +0530

    :poop: shit

--- lib/puppeteer.ts
+++ lib/puppeteer.ts
@@ -2,23 +2,11 @@ import chrome from "chrome-aws-lambda";
 import core from "puppeteer-core";
 
 const launchPuppeteer = async () => {
-  const browser = await core.launch(
-    process.env.NODE_ENV === "production"
-      ? {
-          args: chrome.args,
-          executablePath: await chrome.executablePath,
-          headless: chrome.headless,
-        }
-      : {
-          args: [],
-          executablePath:
-            process.platform === "win32"
-              ? "C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe"
-              : process.platform === "linux"
-              ? "/usr/bin/google-chrome"
-              : "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome",
-        },
-  );
+  const browser = await core.launch({
+    args: chrome.args,
+    executablePath: await chrome.executablePath,
+    headless: chrome.headless,
+  });
   const page = await browser.newPage();
 
   return page;
